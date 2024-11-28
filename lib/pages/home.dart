4033fa4277df973bd25cc0c22b811bc0897efd0d
Jean 28518 <jean28518@github.com>
2024-11-28 11:40:35 +0100

    Add first home page

---
+++ lib/pages/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:privacy_alternatives/widgets/privacy_lights.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Deine privaten Alternativen'),
+        ),
+        body: Column(
+          children: [
+            PrivacyLightsWidget(),
+          ],
+        ));
+  }
+}
---
+++ lib/widgets/privacy_lights.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class PrivacyLightsWidget extends StatefulWidget {
+  const PrivacyLightsWidget({super.key});
+
+  @override
+  _PrivacyLightsWidgetState createState() => _PrivacyLightsWidgetState();
+}
+
+class _PrivacyLightsWidgetState extends State<PrivacyLightsWidget>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat(reverse: true);
+
+    _animation = Tween<double>(begin: -5, end: 5).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    return Padding(
+      padding: EdgeInsets.all(20),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Color.fromRGBO(255, 255, 0, 0.3),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              AnimatedBuilder(
+                animation: _animationController,
+                builder: (context, child) {
+                  return Transform.translate(
+                    offset: Offset(0, _animation.value),
+                    child: child,
+                  );
+                },
+                child: Image.asset(
+                  'assets/images/robbi.png',
+                  width: screenSize.width * 0.2,
+                ),
+              ),
+              Image.asset(
+                'assets/images/lights_yellow.png',
+                height: screenSize.width * 0.4,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Privacy Lights',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  ConstrainedBox(
+                    constraints:
+                        BoxConstraints(maxWidth: screenSize.width * 0.4),
+                    child: Flexible(
+                      child: Text(
+                        'This widget indicates the privacy status using animated lights.',
+                        style: TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
