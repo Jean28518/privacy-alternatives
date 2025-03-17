1121feae432e8b1222b913265677d035285dc1c6
Jean28518 <jf_v@gmx.de>
2025-03-17 00:34:33 +0000

    Add .gitignore and VSCode settings; update AndroidManifest and Gradle files; modify pubspec.yaml dependencies; enhance InstalledAppsService to filter disabled apps

--- lib/services/installed_apps.dart
+++ lib/services/installed_apps.dart
@@ -1,5 +1,8 @@
 import 'package:installed_apps/app_info.dart';
 import 'package:installed_apps/installed_apps.dart';
+import 'package:android_package_manager/android_package_manager.dart';
+
+final pm = AndroidPackageManager();
 
 class InstalledAppsService {
   static List<AppInfo> installedApps = [];
@@ -17,6 +20,24 @@ class InstalledAppsService {
       }
     }
     installedApps = apps;
+
+    // Check in pm if the app is disabled
+    List<AppInfo> apps_to_remove = [];
+    for (var app in installedApps) {
+      // If this fails, change the source dode of the package manager to return a int instead of a bool
+      int? enabledSetting =
+          await pm.getApplicationEnabledSetting(packageName: app.packageName);
+      // Documentation on the constants: https://developer.android.com/reference/android/content/pm/PackageManager#getApplicationEnabledSetting(java.lang.String)
+      if (enabledSetting != null &&
+          (enabledSetting == 2 || enabledSetting == 3)) {
+        print('App ${app.packageName} is disabled');
+        apps_to_remove.add(app);
+      }
+    }
+
+    for (var app in apps_to_remove) {
+      installedApps.remove(app);
+    }
     return apps;
   }
 }
